ad41de9994ee8a561e35bc8802408ef8a155a328
tylerwools79 <tylerwools79@gmail.com>
2020-12-17 11:53:55 -0700

    second part of 12/17/2020 practice

---
+++ Functional/chessTeams.py
@@ -0,0 +1,2 @@
+def chessTeams(smarties, cleveries): #a simple demonstration of zip. matches the ith element in two lists tovether as pairs and returns the list of pairs
+    return zip(smarties,cleveries)
