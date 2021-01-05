2933cca6bb1958a0d3277ce619eee73873d4c60d
tylerwools79 <tylerwools79@gmail.com>
2021-01-05 12:59:52 -0700

    practice for 1/5/2021

---
+++ itertools/crackingPassword.py
@@ -0,0 +1,7 @@
+from itertools import product
+
+def crackingPassword(digits, k, d):
+    def createNumber(digs):
+        return "".join(map(str, digs))
+
+    return sorted([createNumber(x) for x in product(digits,repeat=k)if int(createNumber(x))%d == 0])
